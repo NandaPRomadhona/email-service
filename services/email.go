f730171e44fea6f00891356bd8e950ba7e7a2539
User <user@Userde-MacBook-Pro.local>
2021-12-23 16:10:57 +0800

    add some email patter

---
+++ services/email.go
@@ -0,0 +1,27 @@
+//package services
+//
+//import "fmt"
+//
+//type req1 struct {
+//	Title string `json:"title"`
+//	Body string `json:"body"`
+//}
+//
+//type req2 struct {
+//	Title string `json:"title"`
+//	Body string `json:"body"`
+//	Footer string `json:"footer"`
+//}
+//
+//func doEmail1() interface{} {
+//
+//}
+//
+//func doEmail2() interface{} {
+//
+//}
+//
+//func sendEmail(req interface{}, fn func() interface{})  {
+//	result := req.fn()
+//	fmt.Println(result)
+//}
